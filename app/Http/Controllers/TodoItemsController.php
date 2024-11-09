a61737637370ddf20cbe47fe412b185ad051d84d
unknown <hatehassel@gmail.com>
2024-11-09 08:24:48 +0300

    Initial commit

---
+++ app/Http/Controllers/TodoItemsController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\TodoItem;
+
+class TodoItemsController extends Controller
+{
+    public function getAll() {
+
+        $items = TodoItem::all();
+        return response()->json($items);
+    }
+
+    public function store(Request $request) {
+        $validatedData = $request->validate([
+            "name" => "string|required|max:255", 
+        ]);
+
+        $newItem = TodoItem::create($validatedData); 
+
+        return response()->json($newItem, 201); 
+    }
+
+    public function show($id) {
+        $foundItem = TodoItem::where('id', $id)->first();
+
+        if ($foundItem) {
+            return response()->json($foundItem, 200); 
+        }
+        return response()->json(['message' => 'Item not found'], 404); 
+    }
+
+    public function deleteItem($id) {
+        $foundItem = TodoItem::where('id', $id)->first(); 
+        if ($foundItem) {
+            $foundItem->delete(); 
+            return response()->json(['message' => 'Item deleted successfully'], 200); 
+        }
+        return response()->json(['message' => 'Item not found'], 404); 
+    }
+
+    public function updateItem(Request $request, $id) {
+        $foundItem = TodoItem::where('id', $id)->first(); 
+
+        if ($foundItem) {
+            $request->validate([
+                'name' => 'string|max:255|required',
+            ]);
+
+            $foundItem->name = $request->name; 
+            $foundItem->save(); 
+            return response()->json(['message' => 'Item updated successfully', 'item' => $foundItem]);
+        }
+        return response()->json(['message' => 'Item not found'], 404); 
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\TodoItem;
+use App\Http\Controllers\TodoItemsController; 
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/test', function () {
+    return [
+        'message' => 'test', 
+    ];
+}); 
+
+Route::get('/items', [TodoItemsController::class, 'getAll']);
+Route::post('/create-item', [TodoItemsController::class, 'store']); 
+Route::get('/item/{id}', [TodoItemsController::class, 'show']);
+Route::delete('/item/{id}', [TodoItemsController::class, 'deleteItem']);
+Route::put('/item/{id}', [TodoItemsController::class, 'updateItem']);
